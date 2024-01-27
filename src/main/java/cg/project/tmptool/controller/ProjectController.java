48f2bd0cdd010af462199f5eab465fc48ca807b4
Chen Guo <c82guo@uwaterloo.ca>
2024-01-27 18:38:02 -0500

    Valid BindingResult e.getField e.getDefaultMessage

--- src/main/java/cg/project/tmptool/controller/ProjectController.java
+++ src/main/java/cg/project/tmptool/controller/ProjectController.java
@@ -5,10 +5,15 @@ import cg.project.tmptool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api/project")
@@ -18,7 +23,16 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping(value = "")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            result.getFieldErrors().forEach((e) -> errorMap.put(e.getField(), e.getDefaultMessage()));
+
+
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
         Project newProject = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(newProject, HttpStatus.CREATED);
     }
