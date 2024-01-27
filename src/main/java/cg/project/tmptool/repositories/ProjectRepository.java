0ec4e492c74be10f5677e8e6a65bbb0f727e2fd2
Chen Guo <c82guo@uwaterloo.ca>
2024-01-26 20:28:55 -0500

    project dto and repository

---
+++ src/main/java/cg/project/tmptool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package cg.project.tmptool.repositories;
+
+import cg.project.tmptool.dto.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> longs);
+}
