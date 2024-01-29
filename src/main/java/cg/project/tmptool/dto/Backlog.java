e7f2fccff28ae86c933610326ae53f51059d3a6c
Chen Guo <c82guo@uwaterloo.ca>
2024-01-29 17:51:08 -0500

    bind relationship between Backlog and Task

--- src/main/java/cg/project/tmptool/dto/Backlog.java
+++ src/main/java/cg/project/tmptool/dto/Backlog.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -27,6 +29,9 @@ public class Backlog {
     @JsonIgnore
     private Project project;
 
+
     // OneToMany Tasks
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "backlog")
+    private List<Task> taskList = new ArrayList<>();
 
 }
