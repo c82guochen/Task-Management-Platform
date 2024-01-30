f212cf20a0f2573e859e4b5c2f9e9d22d2104ccb
Chen Guo <c82guo@uwaterloo.ca>
2024-01-29 22:30:24 -0500

    configure MySql setting

--- src/main/java/cg/project/tmptool/services/TaskService.java
+++ src/main/java/cg/project/tmptool/services/TaskService.java
@@ -4,7 +4,6 @@ import cg.project.tmptool.dto.Backlog;
 import cg.project.tmptool.dto.Task;
 import cg.project.tmptool.repositories.BacklogRepository;
 import cg.project.tmptool.repositories.TaskRepository;
-import cg.project.tmptool.exceptions.ProjectNotFoundException;
 import org.hibernate.internal.util.StringHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,32 +20,28 @@ public class TaskService {
     private TaskRepository taskRepository;
 
     public Task addProjectTask(String projectId, Task task) {
-        try {
-            Backlog backlog = backlogRepository.findByProjectId(projectId);
-            task.setBacklog(backlog);
+        Backlog backlog = backlogRepository.findByProjectId(projectId);
+        task.setBacklog(backlog);
 
-            Integer backlogSequence = backlog.getTaskSequence();
-            backlogSequence++;
-            backlog.setTaskSequence(backlogSequence);
+        Integer backlogSequence = backlog.getTaskSequence();
+        backlogSequence++;
+        backlog.setTaskSequence(backlogSequence);
 
-            task.setProjectSequence(projectId + "-" + backlogSequence);
-            task.setProjectId(projectId);
+        task.setProjectSequence(projectId + "-" + backlogSequence);
+        task.setProjectId(projectId);
 
-            if (task.getPriority() == null) {
-                task.setPriority(3);
-            }
-
-            if (StringHelper.isEmpty(task.getStatus())) {
-                task.setStatus("TO_DO");
-            }
-
-            return taskRepository.save(task);
-        } catch (Exception e) {
-            throw new ProjectNotFoundException("Project " + projectId + " does not exist");
+        if (task.getPriority() == null) {
+            task.setPriority(3);
         }
 
+        if (StringHelper.isEmpty(task.getStatus())) {
+            task.setStatus("TO_DO");
+        }
+
+        return taskRepository.save(task);
     }
 
+    // Because we JSONIgnore Backlog in Project, an url should be provided for checking all tasks
     public List<Task> findTaskByProjectId(String productId) {
         return taskRepository.findByProjectIdOrderByPriority(productId);
     }
