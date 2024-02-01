b5154643aa5a9dbe1d070b3f1be347dc5a507b66
Chen Guo <c82guo@uwaterloo.ca>
2024-02-01 08:09:23 -0500

    implement UserDetails

--- src/main/java/cg/project/tmptool/services/UserService.java
+++ src/main/java/cg/project/tmptool/services/UserService.java
@@ -1,7 +1,9 @@
 package cg.project.tmptool.services;
 
+import cg.project.tmptool.dto.User;
 import cg.project.tmptool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +11,13 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder cryptPasswordEncoder;
+
+    public User saveUser(User newUser) {
+        String password = newUser.getPassword();
+        newUser.setPassword(cryptPasswordEncoder.encode(password));
+        return userRepository.save(newUser);
+    }
 }
