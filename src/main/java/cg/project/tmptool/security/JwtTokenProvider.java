dd178ca932e54e4a4a389a10ef2e4cc7e2450201
Chen Guo <c82guo@uwaterloo.ca>
2024-02-01 21:54:31 -0500

    add Jwt validation in provider

--- src/main/java/cg/project/tmptool/security/JwtTokenProvider.java
+++ src/main/java/cg/project/tmptool/security/JwtTokenProvider.java
@@ -1,8 +1,7 @@
 package cg.project.tmptool.security;
 
 import cg.project.tmptool.dto.User;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -39,5 +38,36 @@ public class JwtTokenProvider {
                 .setExpiration(expireDate)
                 .signWith(SignatureAlgorithm.HS512, SECRETE_KEY)
                 .compact();
+    }
+
+    // 2 - Validate the token
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRETE_KEY).parseClaimsJws(token);
+            // 使用Jwts.parser().setSigningKey(SECRETE_KEY)配置JWT解析器，其中SECRETE_KEY是用于验证签名的密钥。
+            // .parseClaimsJws(token)尝试解析和验证传入的token, 如果token有效（即没有抛出任何异常），方法返回true。
+            return true;
+        } catch (SignatureException ex) {
+            System.out.println("Invalid JTW Signature");
+        } catch (MalformedJwtException ex) {
+            System.out.println("Invalid JWT Token");
+        } catch (ExpiredJwtException ex) {
+            System.out.println("Expired JWT Token");
+        } catch (UnsupportedJwtException ex) {
+            System.out.println("Unsupported JWT Token");
+        } catch (IllegalArgumentException ex) {
+            System.out.println("JWT claims string is empty");
+        }
+
+        return false;
+    }
+
+    // 3 - Get user id from token
+    public Long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser().setSigningKey(SECRETE_KEY).parseClaimsJws(token).getBody();
+        // 通过.parseClaimsJws(token).getBody()获取JWT的负载（Claims）
+        String id = (String) claims.get("id");
+
+        return Long.parseLong(id);
     }
 }
